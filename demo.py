d5d660f96ef504056a1933ed05d258839f25661c
dfackler <dfackler@path2response.com>
2018-06-10 18:51:01 -0600

    squares

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@ for i in range(5):
     print('I am a programmer')
 
 # Task no 2: Create a function which displays out the square values of numbers from 1 to 9.
-def square_vals(n):
-    for i in range(1,n):
-        print(i**2)
+def square_vals():
+    for i in range(1,10):
+        print(i*i)
 
 
-square_vals(10)
+square_vals()
