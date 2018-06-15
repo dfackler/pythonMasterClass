75801eb496e9315c1cc08520b610b45d00408615
dfackler <dfackler@path2response.com>
2018-06-14 18:43:03 -0600

    Coding challenge 4, BMI calculator

--- demo.py
+++ demo.py
@@ -1,11 +1,16 @@
-# Task no 1: Using a for-loop and a range function, print "I am a programmer" 5 times.
-for i in range(5):
-    print('I am a programmer')
+# Coding challenge part 4
 
-# Task no 2: Create a function which displays out the square values of numbers from 1 to 9.
-def square_vals():
-    for i in range(1,10):
-        print(i*i)
+# Create a BMI calculator, BMI which stands for Body Mass Index can be calculated using the formula:
 
+#  BMI = (weight in Kg)/(Height in Meters)^2.
 
-square_vals()
+# Write python code which can accept the weight and height of a person and calculate his BMI.
+
+# note: Make sure to use a function which accepts the height and weight values and returns the BMI value.
+
+def bmiCalc(weight, height):
+    bmi = weight/(height**2)
+    return(bmi)
+
+jeff_bmi = bmiCalc(56, 2.8)
+print("Jeff's BMI is " + str(round(jeff_bmi, 3)))
