70509b6d789e0801d13031ac9662e672d454eb78
dfackler <dfackler@path2response.com>
2019-06-09 21:05:21 -0600

    swapped False to None for exceptions

--- demo.py
+++ demo.py
@@ -1,3 +1,23 @@
 def simpleAdd(n1, n2):
     answer = n1 + n2
     return(answer)
+
+def simpleDiv(numerator, divisor):
+    try:
+        answer = numerator/float(divisor)
+        print answer
+        return(answer)
+    except ZeroDivisionError:
+        print "Cannot enter 0 as divisor"
+        return(None)
+    except TypeError:
+        print "Only numerical values are accepted"
+        return(None)
+
+def sumDigits(s):
+    try:
+        answer = len(s)
+        return(answer)
+    except TypeError:
+        print "requires string"
+        return(None)
--- test.py
+++ test.py
@@ -9,5 +9,20 @@ class TestAddMethods(unittest.TestCase):
     def test_positives(self):
         self.assertEqual(demo.simpleAdd(1,2), 3)
 
+    def test_norm_div(self):
+        self.assertAlmostEqual(demo.simpleDiv(1,2), .5)
+
+    def test_zero_div(self):
+        self.assertEqual(demo.simpleDiv(1,0), None)
+    
+    def test_string_div(self):
+        self.assertEqual(demo.simpleDiv("hello", 2), None)
+
+    def test_string_count(self):
+        self.assertEqual(demo.sumDigits("12345"), 5)
+    
+    def test_int_count(self):
+        self.assertEqual(demo.sumDigits(12345), None)
+
 if __name__ == '__main__':
     unittest.main()
