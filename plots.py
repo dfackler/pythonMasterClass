3c153fa56909cc193e908b9ba06c368cf0427243
dfackler <dfackler@path2response.com>
2019-08-11 14:11:06 -0600

    some plots for distances

--- plots.py
+++ plots.py
@@ -1,4 +1,5 @@
 from matplotlib import pylab
+import random
 
 pylab.figure(1) #create figure 1
 # pylab.plot([1,2,3,4], [1,7,3,5]) #draw on figure 1
@@ -17,4 +18,14 @@ pylab.title('5% Growth, Compounded Anually', fontsize = 'xx-large')
 pylab.xlabel('Years of Compounding')
 pylab.ylabel('Value of Principal ($)')
 
+#pylab.show()
+
+
+pylab.figure(2)
+vals = [1,200]
+for i in range(1000):
+    num1 = random.choice(range(1, 100))
+    num2 = random.choice(range(1, 100))
+    vals.append(num1+num2)
+pylab.hist(vals, bins = 10)
 pylab.show()
